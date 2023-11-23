1cb4ae6aadbdde512fecb7811965b62a5d409424
Wes Nicol <38409659+wesnicol@users.noreply.github.com>
2023-11-23 11:39:36 -0700

    changes

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pyudev
 import subprocess
 import logging
+import time
 
 # Constants
-LOG_FILE_PATH = './logs/logfile.log'
-DESTINATION_PATH = '/mnt/media-drive/Media/blu-ray-archive'  # Replace with desired destination path
+LOG_FILE_PATH = '/path/to/logfile.log'
+DESTINATION_PATH = '/path/to/destination'  # Replace with desired destination path
 
 def detect_blu_ray_insertion():
     context = pyudev.Context()
@@ -14,6 +15,7 @@ def detect_blu_ray_insertion():
     for device in iter(monitor.poll, None):
         if 'ID_CDROM' in device.get('ID_CDROM', ''):
             if device.action == 'add':
+                logging.info("Blu-ray disk detected.")
                 return device.device_node
 
 def copy_blu_ray_contents(device_node, destination_path):
@@ -27,17 +29,19 @@ def copy_blu_ray_contents(device_node, destination_path):
     logger.addHandler(file_handler)
 
     try:
-        # Actual copying operation
+        logging.info(f"Copying Blu-ray contents from {device_node} to {destination_path}...")
         subprocess.run(['dd', f'if={device_node}', f'of={destination_path}'])
 
-        logger.info(f"Blu-ray contents copied successfully to {destination_path}")
+        logging.info(f"Blu-ray contents copied successfully to {destination_path}")
     except subprocess.CalledProcessError as e:
-        logger.error(f"Error while copying Blu-ray contents: {e}")
+        logging.error(f"Error while copying Blu-ray contents: {e}")
     except Exception as ex:
-        logger.error(f"An unexpected error occurred: {ex}")
+        logging.error(f"An unexpected error occurred: {ex}")
 
 if __name__ == "__main__":
     while True:
+        logging.info("Waiting for a Blu-ray disk to be inserted...")
         inserted_device = detect_blu_ray_insertion()
         if inserted_device:
             copy_blu_ray_contents(inserted_device, DESTINATION_PATH)
+        time.sleep(5)  # Adjust the delay as needed
